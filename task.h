05be99e263ed72ec78a7e52e90bb5ab007cd1de7
Arthur Rodrigues <ara@cesar.school>
2026-08-20 22:58:39 -0300

    feat: define struct e funçôes no task.h

---
+++ task.h
@@ -0,0 +1,17 @@
+#ifndef TASK_H
+#define TASK_H
+
+#define MAX_ARGS 100
+
+typedef struct Task{
+    char nome[50];
+    char programa[100];
+    char *args[MAX_ARGS];
+    int totalargs;
+} Task;
+
+void cadastrartask(char *nome, char *programa, char *args[], int totalargs);
+
+Task *buscartask(char *nome);
+
+#endif
