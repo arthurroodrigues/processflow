0473f0467b191eb512c13b0dd5ef2fea9900a2ef
Arthur Rodrigues <ara@cesar.school>
2026-08-21 17:10:15 -0300

    fix: adiciona limites de tarefas e argumentos

--- task.c
+++ task.c
@@ -9,6 +9,17 @@ Task listatasks[MAX_TASKS];
 int totaltasks = 0;
 
 void cadastrartask(char *nome, char *programa, char **args, int totalargs) {
+    
+    if (totaltasks >= MAX_TASKS) {
+        printf("Erro: limite de tarefas atingido.\n");
+        return;
+    }
+
+    if (totalargs > MAX_ARGS) {
+        printf("Erro: limite de argumentos atingido.\n");
+        return;
+    }
+
     strcpy(listatasks[totaltasks].nome, nome);
     strcpy(listatasks[totaltasks].programa, programa);
     for (int i = 0; i < totalargs; i++) {
